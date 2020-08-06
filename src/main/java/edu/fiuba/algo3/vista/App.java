e994c31920312d922460dee801699c3010f36ed8
Kevin <kevingadacz@gmail.com>
2020-08-05 21:37:21 -0300

    Agrego logica para validar que se ingrese un nombre y paso los nombres a la pantalla de juego

--- src/main/java/edu/fiuba/algo3/vista/App.java
+++ src/main/java/edu/fiuba/algo3/vista/App.java
@@ -1,6 +1,8 @@
 package edu.fiuba.algo3.vista;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -13,8 +15,24 @@ import javafx.scene.layout.GridPane;
 
 public class App extends Application {
 
-    private Scene getGame(Stage stage) {
+    private Scene getGame(Stage stage,String nombreJugador1, String nombreJugador2) {
         GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(30, 30, 30, 30));
+
+        Text scenetitle = new Text("Bienvenido al juego");
+
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label lblJugador1 = new Label("Nombre jugador 1: " + nombreJugador1);
+        grid.add(lblJugador1, 0, 2);
+
+        Label lblJugador2 = new Label("Nombre jugador 2: " + nombreJugador2);
+        grid.add(lblJugador2, 0, 3);
+
         return new Scene(grid, 350, 250);
     }
 
@@ -44,11 +62,21 @@ public class App extends Application {
 
         Button btnRegistrar = new Button("Registrar");
 
-        btnRegistrar.setOnAction(e -> stage.setScene(getGame(stage)));
+        btnRegistrar.setOnAction(eventoRegistrarJugador(stage,txtJugador1,txtJugador2));
 
         grid.add(btnRegistrar, 1, 4);
 
         return new Scene(grid, 350, 250);
+    }
+
+    private EventHandler<ActionEvent> eventoRegistrarJugador(Stage stage, TextField jugador1, TextField jugador2) {
+        return new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                if (jugador1.getText().length() > 0 && jugador2.getText().length() > 0)
+                    stage.setScene(getGame(stage,jugador1.getText(),jugador2.getText()));
+            }
+        };
     }
 
     @Override
